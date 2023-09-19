680ade0ea51d4c120bae4721ed56bb6d20882409
Walter Rosales <walter.rosales@popoyan.com.gt>
2023-09-19 15:04:46 -0600

    cambio de titulo principal

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('holamundo', function(){
-    return view('holamundo',['nombre'=>'Mundo Laravel']);
+    return view('holamundo',['nombre'=>'Inicia sistema de inventario']);
 });
 
 
